e50765bf0e94f0e6e9d756f4eba68debb7a23f48
Lora Kulm <lkulm@phunware.com>
2016-10-10 16:34:56 -0500

    Added in button, updated page formatting. Got gif working through Glide, button links to starbucks website. Pumpkin spice history now scrolls

--- app/src/main/java/com/example/creativeadmin/testapp/MainActivity.java
+++ app/src/main/java/com/example/creativeadmin/testapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.creativeadmin.testapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.net.Uri;
 
 import com.bumptech.glide.Glide;
 
@@ -13,9 +17,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imageView = (ImageView) findViewById(R.id.dancing);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
 
-        Glide.with(this).load("http://goo.gl/gEgYUd").into(imageView);
+        Glide.with(this).load(R.drawable.fall).asGif().into(imageView);
+
+        final Button button = (Button) findViewById(R.id.pumpSpiceMe);
+
+        button.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.starbucks.com/menu/drinks/espresso/pumpkin-spice-latte"));
+
+                startActivity(browserIntent);
+
+
+            }
+
+        });
+
 
     }
 }
